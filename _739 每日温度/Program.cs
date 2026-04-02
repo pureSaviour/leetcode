3890f1796a3b13c0422f722ba3d8a96a10c30a90
Knight <779284315@qq.com>
2026-04-02 09:48:18 +0800

    leetcode 739 常规单调栈

---
+++ _739 每日温度/Program.cs
@@ -0,0 +1,42 @@
+﻿Solution sol = new();
+int[] temperature = [73,74,75,71,69,72,76,73];
+var ans = sol.DailyTemperatures(temperature);
+foreach (var an in ans)
+{
+    Console.WriteLine(an);
+}
+
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures) {
+        int n = temperatures.Length;
+        Stack<TemperatureInfo> stack = new(n);
+        int[] ans = new int[n];
+        for (int i = 0; i < n; ++i)
+        {
+            while (stack.Count != 0 && temperatures[i] > stack.Peek().Temperature)
+            {
+                var top = stack.Pop();
+                ans[top.Index] = i - top.Index;
+            }
+            stack.Push(new TemperatureInfo()
+            {
+                Temperature = temperatures[i],
+                Index = i
+            });
+        }
+
+        while (stack.Count != 0)
+        {
+            var top = stack.Pop();
+            ans[top.Index] = 0;
+        }
+        return ans;
+    }
+    
+    private readonly struct TemperatureInfo
+    {
+        public int Temperature { get; init; }
+        public int Index { get; init; }
+    }
+}
