9049ac1dbe94794178a2e3ec1a502af653b36d66
Knight <779284315@qq.com>
2026-04-03 11:34:18 +0800

    leetcode 007 要注意整数的诸多限制，比如Abs不可以对minValue取绝对值，同时通过maxValue/10的方法提前判断有没有溢出

---
+++ 007 整数反转/Program.cs
@@ -0,0 +1,24 @@
+﻿Solution solution = new Solution();
+int n = 1563847412;
+Console.WriteLine(n);
+Console.WriteLine(solution.Reverse(n));
+
+
+public class Solution {
+    public int Reverse(int x)
+    {
+        if(x == int.MinValue) return 0;
+        int a = 0;
+        bool isOpposite = x > 0;
+        x = Math.Abs(x);
+        while (x > 0)
+        {
+            if((int.MaxValue - x % 10) / 10 < a) return 0;
+            a = a * 10 + x % 10;
+            x /= 10;
+        }
+
+        
+        return isOpposite ? a : -a;
+    }
+}
