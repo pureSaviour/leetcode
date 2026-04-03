d930f043e06206acebb66acf4c3d7927de7a4688
Knight <779284315@qq.com>
2026-04-03 10:49:54 +0800

    leetcode 2485 数学题，利用整数的取整的性质

---
+++ 2485 找出中枢整数/Program.cs
@@ -0,0 +1,12 @@
+﻿Solution solution = new Solution();
+Console.WriteLine(solution.PivotInteger(4));
+
+public class Solution {
+    public int PivotInteger(int n)
+    {
+        int sum = (n * n + n) / 2;
+        int x = (int)Math.Sqrt(sum);
+        if (x * x == sum) return x;
+        return -1;
+    }
+}
