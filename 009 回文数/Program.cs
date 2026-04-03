84dfef0bd08ea81809071b4566489e4590472008
Knight <779284315@qq.com>
2026-04-03 11:11:48 +0800

    leetcode 009 核心思想是循环优化，对于回文形式的判断，只需要循环一般，强化边界条件的判断（分奇偶）

--- 009 回文数/Program.cs
+++ 009 回文数/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Solution solution = new Solution();
+Console.WriteLine(solution.IsPalindrome(121));
+
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if(x < 0) return false;
+        if(x ==0) return true;
+        if(x % 10 == 0) return false;
+        int original = x;
+        long m = 0;
+        while (x > m)
+        {
+            m = m * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return m == x || m / 10 == x;
+    }
+}
