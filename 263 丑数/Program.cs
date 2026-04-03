6d57f87c2ca1784863af17a07ce70beea6c96b6e
Knight <779284315@qq.com>
2026-04-03 12:45:07 +0800

    leetcode 263 数学题

---
+++ 263 丑数/Program.cs
@@ -0,0 +1,20 @@
+﻿Solution sol = new();
+var ugly = sol.IsUgly(14);
+Console.WriteLine(ugly);
+public class Solution {
+    public bool IsUgly(int n)
+    {
+        int[] cons = [5, 3, 2];
+        if (n is 0 or 1) return false;
+        foreach (var con in cons)
+        {
+            while (n % con == 0)
+            {
+                n /= con;
+            }
+        }
+
+        if (n == 1) return true;
+        return false;
+    }
+}
