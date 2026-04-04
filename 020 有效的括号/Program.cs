663e490bcb405e2e7f7172f4273b8b0eb91efd99
Knight <779284315@qq.com>
2026-04-05 00:13:53 +0800

    leetcode 020 栈

---
+++ 020 有效的括号/Program.cs
@@ -0,0 +1,32 @@
+﻿Solution sol = new Solution();
+var str = "()[]{}";
+var isValid = sol.IsValid(str);
+Console.WriteLine(isValid);
+
+public class Solution {
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (var c in s)
+        {
+            if(c is '(' or '[' or '{')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (c is ')' or ']' or '}')
+                {
+                    if (stack.Count == 0) return false;
+                    char top = stack.Pop();
+                    if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{'))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return stack.Count == 0;
+    }
+}
