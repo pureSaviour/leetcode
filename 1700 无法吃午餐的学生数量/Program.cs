68b6f699f03d740a376f7d4caed38b82e4ae5c88
Knight <779284315@qq.com>
2026-04-07 10:49:00 +0800

    leetcode 014 队列

---
+++ 1700 无法吃午餐的学生数量/Program.cs
@@ -0,0 +1,20 @@
+﻿Solution sol = new();
+var students = new int[] {1,1,0,0}; 
+var sandwiches = new int[] {0,1,0,1};
+var isValid = sol.CountStudents(students, sandwiches);
+Console.WriteLine(isValid);
+
+public class Solution {
+    public int CountStudents(int[] students, int[] sandwiches)
+    {
+        int[] counts = [0, 0];
+        foreach (var student in students)
+        {
+            counts[student]++;
+        }
+        for (int i = 0; i < sandwiches.Length; i++) {
+            if (--counts[sandwiches[i]] == -1) return sandwiches.Length - i;
+        }
+        return 0;
+    }
+}
