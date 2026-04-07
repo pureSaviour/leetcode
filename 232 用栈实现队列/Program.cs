ce6bacfa34a711239e6196e6fecc89e613f591cd
Knight <779284315@qq.com>
2026-04-07 19:24:14 +0800

    leetcode 232 队列

---
+++ 232 用栈实现队列/Program.cs
@@ -0,0 +1,30 @@
+﻿public class MyQueue {
+    private readonly Stack<int> _stackIn = new();
+    private readonly Stack<int> _stackOut = new();
+
+    public void Push(int x) {
+        _stackIn.Push(x);
+    }
+    
+    public int Pop() {
+        if (_stackOut.Count != 0) return _stackOut.Pop();
+        while (_stackIn.Count > 0)
+        {
+            _stackOut.Push(_stackIn.Pop());
+        }
+        return _stackOut.Pop();
+    }
+    
+    public int Peek() {
+        if (_stackOut.Count != 0) return _stackOut.Peek();
+        while (_stackIn.Count > 0)
+        {
+            _stackOut.Push(_stackIn.Pop());
+        }
+        return _stackOut.Peek();
+    }
+    
+    public bool Empty() {
+        return _stackIn.Count == 0 && _stackOut.Count == 0;
+    }
+}
