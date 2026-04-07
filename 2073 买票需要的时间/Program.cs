50011e7fedc69e4dfa6f76f0a40474071b5978f7
Knight <779284315@qq.com>
2026-04-07 19:08:59 +0800

    leetcode 2073 队列

---
+++ 2073 买票需要的时间/Program.cs
@@ -0,0 +1,25 @@
+﻿Solution sol = new();
+var tickets = new int[] {2, 3, 2};
+var k = 2;
+var time = sol.TimeRequiredToBuy(tickets, k);
+Console.WriteLine(time);
+
+public class Solution {
+    public int TimeRequiredToBuy(int[] tickets, int k) {
+        int target = tickets[k];
+        int n = tickets.Length;
+        int time = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i <= k)
+            {
+                time += Math.Min(target, tickets[i]);
+            }
+            else
+            {
+                time += Math.Min(target - 1, tickets[i]);
+            }
+        }
+        return time;
+    }
+}
