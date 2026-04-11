cd9172c9372cd80f5adb7127c07628e61b7cd3a9
Knight <779284315@qq.com>
2026-04-11 13:29:01 +0800

    leetcode 3740 数组容量小，不用哈希表，用两个数组维护索引 + 贪心

---
+++ 3740 三个相等元素之间的最小距离/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution {
+    public int MinimumDistance(int[] nums)
+    {
+        var n = nums.Length;
+        var firstIndex = new int[101];
+        var secondIndex = new int[101];
+        int minDis = int.MaxValue;
+        for (int i = 0; i < firstIndex.Length; i++)
+        {
+            firstIndex[i] = -1;
+        }
+        for (var i = 0; i < n; i++)
+        {
+            var index = nums[i];
+            if (firstIndex[index] == -1)
+            {
+                firstIndex[index] = i;
+            }else
+            {
+                if(secondIndex[index] == 0)
+                    secondIndex[index] = i;
+                else
+                {
+                    minDis = Math.Min(minDis, 2 * (i - firstIndex[index]));
+                    firstIndex[index] = secondIndex[index];
+                    secondIndex[index] = i;
+                }
+            }
+        }
+        return minDis == int.MaxValue ? -1 : minDis;
+    }
+}
