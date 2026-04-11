c8d07284cc3d2b5d713fd9599bf04a265deca5aa
Knight <779284315@qq.com>
2026-04-11 13:26:11 +0800

    leetcode 3741 范围大了，数组初始化时间长，改用哈希表

---
+++ 3741 三个相等元素之间的最小距离2/Program.cs
@@ -0,0 +1,38 @@
+﻿Solution sol = new();
+int[] nums = [1, 2, 1, 1, 3];
+var res = sol.MinimumDistance(nums);
+Console.WriteLine(res);
+
+public class Solution {
+    public int MinimumDistance(int[] nums)
+    {
+        int minDis = int.MaxValue;
+        var dic = new Dictionary<int, Nums>();
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var index = nums[i];
+            if (dic.TryGetValue(index, out var num))
+            {
+                if(num.Second != 0)
+                {
+                    minDis = Math.Min(minDis, 2 * (i - num.First));
+                    num.First = num.Second;
+                }
+                num.Second = i;
+            }
+            else
+            {
+                dic.Add(index, new Nums(i, 0));
+            }
+        }
+        
+        return minDis == int.MaxValue ? -1 : minDis;
+    }
+
+    private sealed class Nums(int first, int second)
+    {
+        public int First { get; set; } = first;
+        public int Second { get; set; } = second;
+    }
+}
