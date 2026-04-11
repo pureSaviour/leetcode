50cfd453b08a656b6474d67e3e4c217fee170543
Knight <779284315@qq.com>
2026-04-11 16:45:18 +0800

    leetcode 1512 太简单

---
+++ 1512 好数对的数目/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public int NumIdenticalPairs(int[] nums)
+    {
+        int n = nums.Length;
+        int[] counts = new int[101];
+        int count = 0;
+        for (int i = 0; i < n; i++)
+            counts[nums[i]]++;
+        foreach (var t in counts)
+            if (t > 1)
+                count += t * (t - 1) / 2;
+
+        return count;
+    }
+    
+}
