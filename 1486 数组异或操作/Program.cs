1795c902155b0a6bedba9a2e084a258b4939e893
Knight <779284315@qq.com>
2026-04-11 16:38:38 +0800

    *leetcode 1486 位运算的性质，还有更好的办法

--- 1486 数组异或操作/Program.cs
+++ 1486 数组异或操作/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Solution sol = new();
+var start = 0;
+var n = 5;
+var res = sol.XorOperation(n, start);
+Console.WriteLine(res);
+
+public class Solution {
+    public int XorOperation(int n, int start) {
+        var res = start;
+        var num = start;
+        for(int i = 1; i < n; ++i){
+            num += 2;
+            res ^= num;
+        }
+        return res;
+    }
+}
