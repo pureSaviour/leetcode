efc15c1b1fb60f0136d03e8fbadb974a7aa45e0b
Knight <779284315@qq.com>
2026-04-13 20:31:31 +0800

    leetcode 1848 太简单

---
+++ 1848 到目标元素的最小距离/Class1.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int GetMinDistance(int[] nums, int target, int start) {
+        int minDistance = int.MaxValue;
+        int n = nums.Length;
+        for (int i = 0; i < n; ++i)
+            if (nums[i] == target)
+            {
+                var dis = Math.Abs(start - i);
+                if(dis < minDistance)
+                    minDistance = dis;
+            }
+        return minDistance;
+    }
+}
