89e5b9831d39518f3d56258c1d49952139146631
Knight <779284315@qq.com>
2026-04-14 16:30:43 +0800

    * leetcode 1354 难，要注意int的溢出，从逆向角度能想到用堆，正向很难理解

---
+++ 1354 多次求和构造目标数组/Program.cs
@@ -0,0 +1,28 @@
+﻿Solution sol = new();
+int[] target = [8, 5];
+var res = sol.IsPossible(target);
+Console.WriteLine(res.ToString());
+
+public class Solution {
+    public bool IsPossible(int[] target)
+    {
+        Int128 sum = 0;
+        var list = new List<(int, int)>(target.Length);
+        foreach (var item in target)
+        {
+            sum += item;
+            list.Add((item, -item));
+        }
+        var pq = new PriorityQueue<int, int>(list);
+        while (pq.Peek() > 1) 
+        {
+            var top = pq.Dequeue();
+            sum -= top;
+            if (sum == 0 || top <= sum) return false;
+            top = (int)((top - 1) % sum + 1);
+            sum += top;
+            pq.Enqueue(top, -top);
+        }
+        return true;
+    }
+}
