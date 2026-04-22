4b37d7c3b30a7cf49984e4ae007e057b21adfab0
Knight <779284315@qq.com>
2026-04-22 17:04:25 +0800

    leetcode 2452 这里用的暴力枚举，但好像还有字典树这种方法还没研究

---
+++ 2452 距离字典两次编辑以内的单词/Program.cs
@@ -0,0 +1,26 @@
+﻿Solution sol = new();
+string[] queries = ["word","note","ants","wood"];
+string[] dictionary = ["wood","joke","moat"];
+var res = sol.TwoEditWords(queries, dictionary);
+foreach (var word in res)    Console.WriteLine(word);
+
+public class Solution {
+    public IList<string> TwoEditWords(string[] queries, string[] dictionary) {
+        List<string> res = new(queries.Length);
+        foreach (var query in queries)
+        foreach (var word in dictionary)
+            if (IsTwoEditDistance(query, word))
+            {
+                res.Add(query);
+                break;
+            }
+        return res;
+    }
+    
+    private static bool IsTwoEditDistance(string s1, string s2)
+    {
+        for (int i = 0, count = 0; i < s1.Length; i++)
+            if (s1[i] != s2[i] && ++count > 2) return false;
+        return true;
+    }
+}
