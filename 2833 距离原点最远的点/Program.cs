23c4288a2aac99b94e876418e46b3c6fddd5cec9
Knight <779284315@qq.com>
2026-04-24 10:25:52 +0800

    leetcode 2833 简单

---
+++ 2833 距离原点最远的点/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public int FurthestDistanceFromOrigin(string moves) {
+        int l = 0, r = 0, _ = 0;
+        foreach (var c in moves)
+            switch (c)
+            {
+                case 'L':
+                    ++l; break;
+                case 'R':
+                    ++r; break;
+                case '_':
+                    ++_; break;
+                default:
+                    throw new NotImplementedException();
+            }
+        return Math.Abs(l - r) + _;
+    }
+}
