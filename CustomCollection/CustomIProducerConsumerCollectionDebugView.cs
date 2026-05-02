b5833c8ec81ea22541baff6ca6d527a8f482a964
Knight <779284315@qq.com>
2026-05-02 16:14:27 +0800

    为IProducerConsumerCollection的Debug提供了类型代理

---
+++ CustomCollection/CustomIProducerConsumerCollectionDebugView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace CustomCollection;
+
+public class CustomIProducerConsumerCollectionDebugView<T>
+{
+    private readonly IProducerConsumerCollection<T> _collection;
+
+    public CustomIProducerConsumerCollectionDebugView(IProducerConsumerCollection<T> collection)
+    {
+        ArgumentNullException.ThrowIfNull(collection);
+        
+        _collection = collection;
+    }
+    [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public T[] Items => _collection.ToArray();
+}
