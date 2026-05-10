b6af313274b8cc6c5a89ab5569342e66e1dcb0fd
Knight <779284315@qq.com>
2026-05-10 09:06:15 +0800

    182双周赛 1 模拟

---
+++ 101043 分数验证器/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution {
+    public int[] ScoreValidator(string[] events)
+    {
+        int count = 0;
+        int score = 0;
+
+        foreach (var @event in events)
+        {
+            if (@event == "W")
+            {
+                if(++count == 10)
+                    break;
+            }
+            else if (@event is "WD" or "NB")
+                ++score;
+            else score += int.Parse(@event);
+        }
+        
+        return [score, count];
+    }
+}
