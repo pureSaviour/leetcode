9bc4b9cfa2ba2a381e057c721b82003b08e93fc7
Knight <779284315@qq.com>
2026-05-10 09:21:08 +0800

    182双周赛 2 模拟

---
+++ 100906 使二进制字符串连贯的最少翻转次数/Program.cs
@@ -0,0 +1,11 @@
+﻿// 100906 使二进制字符串连贯的最少翻转次数
+
+public class Solution {
+    public int MinFlips(string s)
+    {
+        int n = s.Length;
+        int c0 = s.Count(x => x == '0');
+        
+        return Math.Min(c0, Math.Max((s[0] == '1' && s[n - 1] == '1') ? n - c0 - 2 : n - c0 - 1, 0));
+    }
+}
