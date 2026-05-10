456b14a5f28d5a5da466046c5a5e179fd6859f4c
Knight <779284315@qq.com>
2026-05-10 09:01:05 +0800

    leetcode 2770 dp

---
+++ 2770 达到末尾下标所需的最大跳跃次数/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public int MaximumJumps(int[] nums, int target) {
+        int n = nums.Length;
+        int[] dp = new int[n];
+        Array.Fill(dp, int.MinValue);
+        dp[0] = 0;
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (Math.Abs(nums[j] - nums[i]) <= target) {
+                    dp[i] = Math.Max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        
+        return dp[n - 1] < 0 ? -1 : dp[n - 1];
+    }
+}
