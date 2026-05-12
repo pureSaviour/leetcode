5d0ef7dfd9bebfc31bed54c4d937a6123ab3af58
Knight <779284315@qq.com>
2026-05-12 11:26:16 +0800

    leetcode 1665 贪心

---
+++ 1665 完成所有任务的最少初始能量/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public int MinimumEffort(int[][] tasks) {
+        Array.Sort(tasks, (a, b) => (a[1] - a[0]).CompareTo(b[1] - b[0]));
+        int ans = 0;
+        foreach (int[] task in tasks) {
+            ans = Math.Max(ans + task[0], task[1]);
+        }
+        return ans;
+    }
+}
