a123b497f8049ed404989096e3d0f13cf95bd693
Knight <779284315@qq.com>
2026-05-15 22:18:08 +0800

    leetcode 153 二分

--- 153 寻找旋转排序数组中的最小值/Program.cs
+++ 153 寻找旋转排序数组中的最小值/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] nums = [2, 1];
+Solution sol = new();
+Console.WriteLine(sol.FindMin(nums));
+
+public class Solution {
+    public int FindMin(int[] nums)
+    {
+        int n = nums.Length;
+        int l = 0;
+        int r = n - 1;
+        while (l < r)
+        {
+            int m = l + (r - l) / 2;
+            if (nums[m] < nums[r])
+                r = m;
+            else
+                l = m + 1;
+        }
+        return nums[r];
+    }
+}
