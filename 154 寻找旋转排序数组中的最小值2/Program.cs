b7d51031e4d51ac724a4222958b6e2801ec642c8
Knight <779284315@qq.com>
2026-05-16 17:04:05 +0800

    *leetcode 154 二分+相等值,细节比较多

---
+++ 154 寻找旋转排序数组中的最小值2/Program.cs
@@ -0,0 +1,34 @@
+﻿Solution sol = new();
+int[] nums = [2,0,1,1,1];
+Console.WriteLine(sol.FindMin(nums));
+
+public class Solution {
+    public int FindMin(int[] nums)
+    {
+        int n = nums.Length;
+        int l = 0;
+        int r = n - 1;
+        while (l < r)
+        {
+            int m = l + (r - l) / 2;
+            if (nums[m] == nums[l] && nums[m] == nums[r])
+                ++l;
+            else if (nums[l] > nums[r])
+            {
+                if (nums[m] <= nums[r])
+                    r = m;
+                else 
+                    l = m + 1;
+            }
+            else
+            {
+                if (nums[m] <= nums[r])
+                    r = m;
+                else
+                    l = m + 1;
+            }
+        }
+
+        return nums[r];
+    }
+}
