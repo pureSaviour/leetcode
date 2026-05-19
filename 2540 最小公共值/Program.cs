b8ebad1abc052b8da11de3bf3ac64a9d13a2edc9
Knight <779284315@qq.com>
2026-05-19 10:24:50 +0800

    leetcode 2540 双指针简单

---
+++ 2540 最小公共值/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public int GetCommon(int[] nums1, int[] nums2)
+    {
+        int index1 = 0;
+        int index2 = 0;
+        while (index1 < nums1.Length && index2 < nums2.Length)
+        {
+            var num1 = nums1[index1];
+            var num2 = nums2[index2];
+            if (num1 == num2)
+                return num1;
+            if (num1 > num2)
+                ++index2;
+            else
+                ++index1;
+        }
+
+        return -1;
+    }
+}
