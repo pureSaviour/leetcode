42b9c9c4a80dc70696fd0c544afddcec5548f651
Knight <779284315@qq.com>
2026-05-20 15:53:37 +0800

    leetcode 2657 有位运算的巧妙方法，但是数据长度只可以有64位以内，还是哈希更泛用一点，效率更高

---
+++ 2657 找到两个数组的前缀公共数组/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Numerics;
+
+Solution sol = new();
+int[] A = [1, 3, 2, 4];
+int[] B = [3, 1, 2, 4];
+var res = sol.FindThePrefixCommonArray(A, B);
+Console.WriteLine(string.Join(", ", res));
+public class Solution {
+    public int[] FindThePrefixCommonArray(int[] A, int[] B)
+    {
+        int n = A.Length;
+        bool[] visited = new bool[n + 1];
+        int[] res = new int[n];
+        for (int i = 0; i < n; ++i)
+        {
+            if(i >= 1)
+                res[i] = res[i - 1];
+            var numA = A[i];
+            var numB = B[i];
+            if (visited[numA])
+                ++res[i];
+            visited[numA] = true;
+            if (visited[numB])
+                ++res[i];
+            visited[numB] = true;
+        }
+        return res;
+    }
+
+    public int[] FindThePrefixCommonArrayByBit(int[] A, int[] B)
+    {
+        int n =  A.Length; 
+        int[] res = new int[n];
+        ulong maskA = 0;
+        ulong maskB = 0;
+        for(int i = 0; i < n; ++i)
+        {
+            maskA |= 1UL << A[i];
+            maskB |= 1UL << B[i];
+            res[i] = BitOperations.PopCount(maskA & maskB);
+        }
+
+        return res;
+    }
+}
