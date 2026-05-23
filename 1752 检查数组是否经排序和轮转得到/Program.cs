7a952c0c493ab1ce00798c86d8f2628d37b73d5e
Knight <779284315@qq.com>
2026-05-23 11:47:01 +0800

    leetcode 1752 理解分段单调的本质即可

---
+++ 1752 检查数组是否经排序和轮转得到/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] nums = [1,3,2];
+Solution sol = new();
+var res = sol.Check(nums);
+Console.WriteLine(res);
+
+public class Solution {
+    public bool Check(int[] nums)
+    {
+        int secondMax = int.MinValue;
+        bool flag = false;
+        for (int i = 1; i < nums.Length; ++i)
+        {
+            if (nums[i] < nums[i - 1])
+            {
+                if (flag)
+                    return false;
+                flag = true;
+                secondMax = nums[i];
+            }
+            else if(flag)
+                secondMax = nums[i];
+        }
+        return secondMax <= nums[0];
+    }
+}
