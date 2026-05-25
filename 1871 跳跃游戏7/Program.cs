5963339055668ad4683742d39c69e309284934b3
Knight <779284315@qq.com>
2026-05-25 15:30:46 +0800

    leetcode 1871 双指针

---
+++ 1871 跳跃游戏7/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public bool CanReach(string s, int minJump, int maxJump) {
+        int n = s.Length;
+        bool[] visited = new bool[n];
+        visited[0] = true;
+        for(int i = 0, j = 1; i < n && j < n; ++i){
+            if(visited[i] && s[i] == '0'){
+                for(j = Math.Max(j, i + minJump); j <= Math.Min(i + maxJump, n - 1);++j){
+                    visited[j] = true;            
+                }
+            }
+        }
+        return visited[n - 1] && s[n -  1] == '0';
+    }
+}
