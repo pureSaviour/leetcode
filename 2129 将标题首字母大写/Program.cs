d131d969009555f8db293076f21e2544127f9ee5
Knight <779284315@qq.com>
2026-05-26 15:05:05 +0800

    leetcode 2129 简单

---
+++ 2129 将标题首字母大写/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public string CapitalizeTitle(string title)
+    {
+        bool newWord = true;
+        string[] words = title.Split(' ');
+        for (int i = 0; i < words.Length; ++i)
+        {
+            words[i] = words[i].ToLower();
+            if (words[i].Length > 2)
+            {
+                var charArray = words[i].ToCharArray();
+                charArray[0] = char.ToUpper(charArray[0]);
+                words[i] = new string(charArray);
+            }
+        }
+        return string.Join(' ', words);
+    }
+}
