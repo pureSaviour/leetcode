ff4879e7a48e36bef96f34659de3fec26c2a5bb6
Knight <779284315@qq.com>
2026-05-26 15:11:48 +0800

    *leetcode 709 字符位运算技巧

---
+++ 709 转换成小写字母/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public string ToLowerCase(string s)
+    {
+        // 大写变小写、小写变大写 : 字符 ^= 32;
+        // 大写变小写、小写变小写 : 字符 |= 32;
+        // 小写变大写、大写变大写 : 字符 &= -33;
+        
+        var charArray = s.ToCharArray();
+        for (int i = 0; i < charArray.Length; ++i)
+            if(char.IsLetter(charArray[i]))
+                charArray[i] |= (char)32;
+        
+        return new string(charArray);
+    }
+}
