cc465fc505a32a2756bafecebd1d19ff8d3157a6
Knight <779284315@qq.com>
2026-05-26 14:46:57 +0800

    leetcode 3120 简单

---
+++ 3120 统计特殊字母的数量1/Program.cs
@@ -0,0 +1,23 @@
+﻿Solution sol = new();
+string word = "aaAbcBC";
+Console.WriteLine(sol.NumberOfSpecialChars(word));
+
+
+public class Solution {
+    public int NumberOfSpecialChars(string word)
+    {
+        bool[] visited = new bool[52];
+        int count = 0;
+        foreach (var c in word)
+        {
+            var index = GetIndex(c);
+            if (index < 26 && visited[index + 26] && !visited[index] || index >= 26 && visited[index - 26] && !visited[index])
+                ++count;
+            visited[index] = true;
+        }
+
+        return count;
+    }
+
+    private static int GetIndex(char c) => char.IsLower(c) ? c - 'a' : c - 'A' + 26;
+}
