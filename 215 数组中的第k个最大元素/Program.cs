93bab34e6e99b2a4c1c71186c6148fa1898500e1
Knight <779284315@qq.com>
2026-05-27 17:45:15 +0800

    leetcode LCR076 随机选择实现On时间复杂度,O1空间复杂度

---
+++ 215 数组中的第k个最大元素/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tool;
+
+public class Solution {
+    public int FindKthLargest(int[] nums, int k)
+    {
+        Comparer<int> comparer = Comparer<int>.Create((x, y) => y - x);
+        return Find.RandomSelect(nums, k, comparer);
+    }
+}
