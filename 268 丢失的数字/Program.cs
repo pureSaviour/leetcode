e3d1b27a136e022a92d52a5bcd7759e6e4403e70
Knight <779284315@qq.com>
2026-05-30 13:42:51 +0800

    *leetcode 异或运算

---
+++ 268 丢失的数字/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Solution {
+    public int MissingNumber(int[] nums)
+    {
+        int eorAll = nums.Length;
+        for (int i = 0; i < nums.Length; ++i)
+            eorAll ^= (i ^ nums[i]);
+        return eorAll;
+    }
+}
