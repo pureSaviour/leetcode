b1a7e220cff5cd95a1ec99b1d9f81f9909644e06
Knight <779284315@qq.com>
2026-05-30 13:49:32 +0800

    leetcode 136 异或运算的性质

---
+++ 136 只出现一次的数字/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Solution {
+    public int SingleNumber(int[] nums) {
+        int eor = 0;
+        for(int i = 0; i < nums.Length; ++i)
+            eor ^= nums[i];
+        return eor;
+    }
+}
