c2c811c27a68af800198a6020ff0ed45d9dfea32
Knight <779284315@qq.com>
2026-05-30 14:28:55 +0800

    leetcode 137 位运算

---
+++ 137 只出现1次的数字2/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public int SingleNumber(int[] nums) {
+        int n = nums.Length;
+        int intSize = sizeof(int) * 8;
+        int[] cnt = new int[intSize];
+        int res = 0;
+        for(int i = 0; i < n; ++i)
+        for (int j = 0; j < intSize; ++j)
+            cnt[j] += (nums[i] >> j) & 1;
+        for(int i = 0; i < intSize; ++i)
+            if (cnt[i] % 3 != 0)
+                res |= (1 << i);
+        return res;
+    }
+}
