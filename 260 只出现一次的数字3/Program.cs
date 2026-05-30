b92d4fd60751886ccc99b657747672db50ec47c4
Knight <779284315@qq.com>
2026-05-30 14:14:09 +0800

    *leetcode 260 异或！

---
+++ 260 只出现一次的数字3/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public int[] SingleNumber(int[] nums) {
+        int n = nums.Length;
+        int eor1 = 0;
+        for(int i = 0; i < n; ++i)
+            eor1 ^= nums[i];
+        int eor2 = 0;
+        int rightOne = eor1 & (-eor1);
+        for(int i = 0; i < n; ++i)
+            if((nums[i] & rightOne) == 0)
+                eor2 ^= nums[i];
+
+        return [eor2, eor1 ^ eor2];
+    }
+}
