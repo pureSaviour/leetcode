00c7f46402f5968b26b1eb15cc3c471f03a3bdf8
Knight <779284315@qq.com>
2026-06-02 11:16:37 +0800

    leetcode 3635 学习了zip linq

---
+++ 3635 最早完成陆地和水上游乐设施的时间2/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public int EarliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int lW = Solve(landStartTime, landDuration, waterStartTime, waterDuration);
+        int wL = Solve(waterStartTime, waterDuration, landStartTime, landDuration);
+        return Math.Min(lW, wL);
+    }
+
+    private static int SolveLinq(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration){
+        int minFinish = landStartTime.Zip(landDuration).Min(item => item.First + item.Second);
+        return waterStartTime.Zip(waterDuration).Min(item => Math.Max(item.First, minFinish) + item.Second);
+    }
+    
+    private static int Solve(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration){
+        int minFinish = int.MaxValue;
+        for(int i = 0; i < landStartTime.Length; ++i)
+            minFinish = Math.Min(minFinish, landStartTime[i] + landDuration[i]);
+        int ans = int.MaxValue;
+        for(int i = 0; i < waterStartTime.Length; ++i)
+            ans = Math.Min(ans, Math.Max(waterStartTime[i], minFinish) + waterDuration[i]);
+        return ans;
+    }
+}
