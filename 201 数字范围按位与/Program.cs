c07098ad5763266f89f01d856396af62d12554c8
Knight <779284315@qq.com>
2026-06-02 18:33:16 +0800

    *leetcode 201 位运算，过于巧妙

---
+++ 201 数字范围按位与/Program.cs
@@ -0,0 +1,7 @@
+﻿public class Solution {
+    public int RangeBitwiseAnd(int left, int right) {
+        while(right > left)
+            right -= (right & -right);    
+        return right;
+    }
+}
