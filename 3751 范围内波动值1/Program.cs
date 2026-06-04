f6b94919492a8f07ab84f11da088019fd57ada09
Knight <779284315@qq.com>
2026-06-04 13:08:47 +0800

    leetcode 3751 暴力

---
+++ 3751 范围内波动值1/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public int TotalWaviness(int num1, int num2)
+    {
+        int sum = 0;
+        for(int i = Math.Max(num1, 100); i <= num2; ++i)
+            sum += GetCount(i);
+        return sum;
+    }
+
+    private static int GetCount(int num){
+        string str = num.ToString();
+        int count = 0;
+        for(int i = 1; i < str.Length - 1; ++i)
+            if ((str[i - 1] < str[i] && str[i] > str[i + 1]) || (str[i - 1] > str[i] && str[i] < str[i + 1]))
+                ++count;
+        return count;
+    }
+}
