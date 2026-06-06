714a084939cddfad24a1c31d2db608f40b6bc5eb
Knight <779284315@qq.com>
2026-06-06 10:12:49 +0800

    leetcode 2574 前缀和

---
+++ 2574 左右元素和的差值/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Solution {
+    public int[] LeftRightDifference(int[] nums) {
+        int[] ans = new int[nums.Length];
+        for (int i = 0, leftSum = 0, sum = nums.Sum(); i < nums.Length; leftSum += nums[i++])
+            ans[i] = Math.Abs(2 * leftSum + nums[i] - sum);
+        return ans;
+    }
+}
