b1cb81105a763314066ce8b408c007de506f9684
Knight <779284315@qq.com>
2026-06-09 11:32:09 +0800

    leetcode 简单

---
+++ 2161 根据给定数字划分数组/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public int[] PivotArray(int[] nums, int pivot)
+    {
+        int index = 0;
+        int[] ans = new int[nums.Length];
+        foreach (var num in nums)
+            if(num < pivot)
+                ans[index++] = num;
+        foreach (var num in nums)
+            if(num == pivot)
+                ans[index++] = num;
+        foreach (var num in nums)
+            if(num > pivot)
+                ans[index++] = num;
+        return ans;
+    }
+}
