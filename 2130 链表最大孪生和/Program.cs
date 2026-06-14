1b1c84583ff916d37f952366c210786719d9fcb3
Knight <779284315@qq.com>
2026-06-14 11:50:20 +0800

    leetcode 2130 快慢指针找链表中点，反转中点后的链表，实现O1空间

---
+++ 2130 链表最大孪生和/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] nums = [5, 4, 2, 1];
+ListNode head = new ListNode(nums[0]);
+ListNode cur = head;
+for (int i = 1; i < nums.Length; ++i)
+{
+    cur.next = new ListNode(nums[i]);
+    cur = cur.next;
+}
+
+Solution sol = new();
+var sum = sol.PairSum(head);
+Console.WriteLine(sum);
+
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+ 
+public class Solution {
+    public int PairSum(ListNode head) {
+        ListNode slow = head, fast = head.next;
+        while(fast.next is not null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = ReverseList(slow.next);
+        int sum = 0;
+        while(fast is not null){
+            sum = Math.Max(sum, head.val + fast.val);
+            head = head.next;
+            fast = fast.next;
+        }
+        return sum;
+    }
+
+    private static ListNode ReverseList(ListNode head){
+        ListNode pre = null!;
+        while(head is not null){
+            var next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
