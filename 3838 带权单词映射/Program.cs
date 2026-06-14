de44863f9468d507f143f0231b0d5b38e5bab0a7
Knight <779284315@qq.com>
2026-06-14 11:34:59 +0800

    leetcode 3838 简单

---
+++ 3838 带权单词映射/Program.cs
@@ -0,0 +1,3 @@
+﻿public class Solution {
+    public string MapWordWeights(string[] words, int[] weights) => new string(words.Select(w => (char)('z' - w.Sum(c => weights[c - 'a']) % 26)).ToArray());
+}
