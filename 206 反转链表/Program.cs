b1b6ef8ce58be70cb8a8471e1be1ef8443728434
Knight <779284315@qq.com>
2026-06-14 11:36:51 +0800

    leetcode 206 迭代的方法很有意思

---
+++ 206 反转链表/Program.cs
@@ -0,0 +1,22 @@
+﻿
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+ 
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode pre = null!;
+        while(head is not null){
+            var next = head.next;
+            head.next = pre;                       
+            pre = head;    
+            head = next;
+        }
+        return pre;
+    }
+}
