ff710021715cf3cbab09d7aad3e70a82dbcc01eb
Knight <779284315@qq.com>
2026-06-16 16:03:02 +0800

    leetcode 3612 题解用StringBuilder，但是翻转的时间复杂度是On，这里用双向链表，时间复杂度更优但是常数时间慢

---
+++ 3612 用特殊操作处理字符串1/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Solution {
+    public string ProcessStr(string s) {
+        LinkedList<char> deque = new LinkedList<char>();
+        bool isRev = false;
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case '%':
+                    isRev = !isRev;
+                    break;
+                case '#':
+                {
+                    char[] charArr = new char[deque.Count];
+                    deque.CopyTo(charArr, 0);
+                    foreach (var t in charArr)
+                        deque.AddLast(t);
+                    break;
+                }
+                case '*':
+                {
+                    if (deque.Count > 0)
+                    {
+                        if(isRev)
+                            deque.RemoveFirst();
+                        else
+                            deque.RemoveLast();
+                    }
+                    break;
+                }
+                default:
+                {
+                    if(isRev)
+                        deque.AddFirst(c);
+                    else
+                        deque.AddLast(c);
+                    break;
+                }
+            }
+        }
+
+        char[] res = deque.ToArray();
+        if(isRev)
+            Array.Reverse(res);
+        return new string(res);
+    }
+}
