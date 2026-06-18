8faddd287f418552b6dfb5c0afacf6051835cd8e
Knight <779284315@qq.com>
2026-06-18 18:37:28 +0800

    leetcode 1344 纯数学

---
+++ 1344 时间指针的夹角/Program.cs
@@ -0,0 +1,7 @@
+﻿public class Solution {
+    public double AngleClock(int hour, int minutes) {
+        double hDegree = (hour % 12 + (minutes % 60) / 60d) * 30;
+        double mDegree = (minutes % 60) * 6d;
+        return Math.Min(Math.Abs(hDegree - mDegree), 360 - Math.Abs(hDegree - mDegree));
+    }
+}
