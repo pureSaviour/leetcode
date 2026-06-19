91f05f1bb9e5efbd110f19e9c70d096c0a19c1b0
Knight <779284315@qq.com>
2026-06-19 21:47:06 +0800

    leetcode 1732 简单

---
+++ 1732 找到最高海拔/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public int LargestAltitude(int[] gain) {
+        int max = 0;
+        for(int i = 0, cur = 0; i < gain.Length; ++i){
+            cur += gain[i];
+            max = Math.Max(cur, max);
+        }
+        return max;
+    }
+}
