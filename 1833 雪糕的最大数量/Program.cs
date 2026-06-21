283815fe9566bf677e8578759a1c31e8edd56ead
Knight <779284315@qq.com>
2026-06-21 11:31:11 +0800

    leetcode 1833 计数排序

---
+++ 1833 雪糕的最大数量/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution {
+    public int MaxIceCream(int[] costs, int coins) {
+        int min = int.MaxValue, max = int.MinValue;
+        foreach(var cost in costs){
+            min = Math.Min(cost, min);
+            max = Math.Max(cost, max);
+        }
+
+        int[] orderedCosts = new int [max - min + 1];
+        foreach(var cost in costs)
+            ++orderedCosts[cost - min];
+        int count = 0;
+        for (int i = 0; i < orderedCosts.Length; ++i)
+        {
+            if (orderedCosts[i] > 0)
+            {
+                int k = coins / (i + min);
+                if (k >= orderedCosts[i])
+                {
+                    coins -= orderedCosts[i] * (i + min);
+                    count += orderedCosts[i];
+                }
+                else
+                {
+                    count += k;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
